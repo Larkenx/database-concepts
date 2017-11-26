61c3c4db7bf7ecae42efd9d04747b33d307fcdc5
Steven Myers <larkenx@gmail.com>
2017-11-26 13:48:56 -0500

    finished q2,q3

--- a7/a7.sql
+++ a7/a7.sql
@@ -20,7 +20,6 @@ CREATE TABLE E_COPY (
 );
 
 -- Transitive Closure function from the lectures
-
 CREATE OR REPLACE FUNCTION new_TC_pairs()
   RETURNS TABLE(source INTEGER, target INTEGER) AS
 $$
@@ -75,11 +74,38 @@ SELECT EXISTS(
 );
 $$ LANGUAGE SQL;
 
-SELECT Transitive_Closure();
 
-SELECT is_not_connected();
+CREATE OR REPLACE FUNCTION test()
+  RETURNS VOID AS $$
+BEGIN
+  TRUNCATE TABLE AC;
+  TRUNCATE TABLE E_COPY;
+  INSERT INTO e_Copy SELECT *
+                     FROM e
+                     WHERE e.source <> 1 AND e.target <> 1;
+  INSERT INTO e_copy VALUES (2, 2), (3, 3);
+  PERFORM Transitive_Closure();
+  IF (SELECT EXISTS(
+      SELECT DISTINCT
+        E1.source,
+        E2.target
+      FROM E_COPY E1, E_COPY E2
+      EXCEPT
+      SELECT
+        TC.source,
+        TC.target
+      FROM TC
+  ))
+  THEN
+    INSERT INTO AC VALUES (1);
+  END IF;
+END
+$$ LANGUAGE PLPGSQL;
 
-
+SELECT test();
+-- Problem 1
+SELECT *
+FROM ac;
 -- PLAN FOR ARTICULATION POINTS:
 -- *Adapt transitive closure functions to work on E_COPY table created in articulation points
 -- LOOP through all vertices. For each vertex:
@@ -88,20 +114,40 @@ SELECT is_not_connected();
 -- If G is not connected, then the vertex is an articulation point. Put it in the list of articulation points
 CREATE OR REPLACE FUNCTION articulation_points()
   RETURNS VOID AS $$
-DECLARE vertex        INTEGER := NULL;
-        not_connected BOOLEAN := FALSE;
+DECLARE vertex  INTEGER := NULL;
+        vertex2 INTEGER := NULL;
 BEGIN
-  FOR vertex IN SELECT DISTINCT source FROM E
+  TRUNCATE TABLE AC;
+  FOR vertex IN (SELECT DISTINCT source
+                 FROM E)
   LOOP
     TRUNCATE TABLE E_COPY;
     INSERT INTO E_COPY SELECT *
                        FROM E
                        WHERE E.source <> vertex AND E.target <> vertex;
+    --- I want to put self-referential edges into the copy of the graph because
+    -- the transitive closure includes self-referential points as well. If there is a graph like
+    -- (1,1) and (2,2), then we have two connected components (the graph isn't connected).
+    FOR vertex2 IN (SELECT DISTINCT source
+                    FROM E_COPY)
+    LOOP
+      INSERT INTO E_COPY VALUES (vertex2, vertex2);
+    END LOOP;
     -- now we need to update our transitive closure, but we just need this to be a side-effect
     PERFORM Transitive_Closure();
     -- if removing the vertex and its edges from E cause it to be disconnected, then
     -- it is an articulation point
-    IF (select is_not_connected())
+    IF (SELECT EXISTS(
+        SELECT DISTINCT
+          E1.source,
+          E2.target
+        FROM E_COPY E1, E_COPY E2
+        EXCEPT
+        SELECT
+          TC.source,
+          TC.target
+        FROM TC
+    ))
     THEN
       INSERT INTO AC VALUES (vertex);
     END IF;
@@ -109,85 +155,133 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-select * from TC;
 SELECT articulation_points();
+SELECT *
+FROM AC;
 
-select * from AC;
-
-
-CREATE TABLE A (
-  n INTEGER
+-- Problem 2
+DROP TABLE IF EXISTS PC;
+CREATE TABLE PC (
+  p INTEGER,
+  c INTEGER
 );
-INSERT INTO A VALUES
-  (1),
-  (2),
-  (3);
+-- values from the expected output
+INSERT INTO PC VALUES
+  (1, 2),
+  (1, 3),
+  (2, 4),
+  (3, 6),
+  (6, 8),
+  (1, 5),
+  (5, 9);
 
+-- Ancestor relationships from lecture
 
-CREATE OR REPLACE FUNCTION powerset(INTEGER [])
-  RETURNS TABLE(subset INTEGER []) AS $$
-DECLARE i   INTEGER := 0;
-        j   INTEGER := 0;
-        v   INTEGER;
-        val INTEGER;
-        arr INTEGER [];
-        ps  INTEGER [] [];
-        tmp INTEGER [] [];
+CREATE OR REPLACE FUNCTION new_ANC_pairs()
+  RETURNS TABLE(A INTEGER, D INTEGER) AS
+$$
+(SELECT
+   A,
+   C
+ FROM ANC, PC
+ WHERE D = P)
+EXCEPT
+(SELECT
+   A,
+   D
+ FROM ANC);
+$$ LANGUAGE SQL;
+CREATE OR REPLACE FUNCTION Ancestor_Descendant()
+  RETURNS TABLE(person INTEGER, descendant INTEGER) AS $$
 BEGIN
-  ps  := '{ {} }';
-  DROP TABLE IF EXISTS powersetA;
-  CREATE TABLE powersetA (
-    subset INTEGER []
+  DROP TABLE IF EXISTS ANC;
+  CREATE TABLE ANC (
+    A INTEGER,
+    D INTEGER
   );
-
-  -- for every item in our array
-  FOREACH val IN ARRAY $1
+  INSERT INTO ANC SELECT *
+                  FROM PC;
+  WHILE exists(SELECT *
+               FROM new_ANC_pairs())
   LOOP
-    RAISE NOTICE 'working with val: (%)', (val);
-    tmp := '{}';
-
-    -- for every powerset we've already added to the powerset
-    FOR i IN 0..array_length(ps, 1)
-    LOOP
-      RAISE NOTICE 'Entering FOR loop through arrays in powerset';
-      RAISE NOTICE 'Array length of PS is %', array_length(ps, 1);
-      --       RAISE NOTICE '{';
-      --       FOREACH v IN ARRAY arr
-      --       LOOP
-      --         RAISE NOTICE '%',  v;
-      --       END LOOP;
-      --       RAISE NOTICE '}';
-
-      tmp := array_append(tmp, ps [i]);
-      tmp := array_append(tmp, array_append(ps [i], val));
-    END LOOP;
-    ps := tmp;
-
-  END LOOP;
-
-  FOREACH arr IN ARRAY ps
-  LOOP
-    RAISE NOTICE 'array value: (%)', (arr);
-    INSERT INTO powersetA VALUES (arr);
+    INSERT INTO ANC SELECT *
+                    FROM new_ANC_pairs();
   END LOOP;
 
   RETURN QUERY (SELECT *
-                FROM powersetA);
-
+                FROM ANC);
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT powerset('{1,2,3}');
 
-SELECT *
-FROM powersetA;
+-- produces a relation of (person, generation) where the generation is the person's depth in the family tree
+CREATE OR REPLACE FUNCTION generations()
+  RETURNS TABLE(person INTEGER, generation INTEGER) AS $$
+DECLARE p              INTEGER; -- current person we want to calculate generation
+        depth          INTEGER;
+BEGIN
+  DROP TABLE IF EXISTS generations;
+  CREATE TABLE generations (
+    person     INTEGER,
+    generation INTEGER
+  );
 
+  -- for each person, their depth is the number of times they appear as a descendant in the ANC relation
+  FOR p IN ((SELECT DISTINCT pc.p FROM pc) UNION (SELECT DISTINCT pc.c FROM pc)) -- all persons
+  LOOP
+      depth := (select count(anc.d) from ANC where anc.d=p);
+      insert into generations values (p, depth);
+  END LOOP;
 
-CREATE TABLE test (
-  mdarr INTEGER [] []
-);
+  RETURN QUERY(select * from generations);
+END;
+$$ LANGUAGE PLPGSQL;
 
-INSERT INTO test VALUES ('{{1,2,3}}')
+select * from generations();
 
-SELECT array_length(mdarr, 1)
-FROM test;
+CREATE OR REPLACE FUNCTION same_gen_pairs()
+  RETURNS TABLE(id1 integer, id2 integer) AS $$
+    select g1.person, g2.person
+    from generations g1, generations g2
+    where g1.generation=g2.generation
+    order by 1,2
+$$ LANGUAGE SQL;
+
+select * from same_gen_pairs();
+
+-- Problem 3
+
+CREATE OR REPLACE FUNCTION powerset(INTEGER [])
+  RETURNS TABLE(subset INTEGER []) AS $$
+DECLARE set INTEGER [];
+        val INTEGER;
+BEGIN
+  DROP TABLE IF EXISTS powerset;
+  DROP TABLE IF EXISTS tmp;
+  CREATE TABLE powerset (
+    subset INTEGER []
+  );
+  CREATE TABLE tmp (
+    subset INTEGER []
+  );
+  INSERT INTO powerset VALUES ('{}');
+  FOREACH val IN ARRAY $1
+  LOOP
+    FOR set IN (SELECT *
+                FROM powerset)
+    LOOP
+      INSERT INTO tmp VALUES (val || set);
+    END LOOP;
+    INSERT INTO powerset (SELECT *
+                          FROM tmp);
+    TRUNCATE tmp;
+  END LOOP;
+
+  RETURN QUERY (SELECT *
+                FROM powerset);
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT powerset('{5,6,7}');
+
+-- Problem 4
